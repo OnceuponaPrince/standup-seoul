dc1a423a3fc5c8e4dcdf5a65dc0ae0332d113b60
Prince John <onceuponaprince@Princes-MacBook-Pro.local>
2021-11-26 09:47:51 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header/Header.js';
+import FooterLogo from './assets/alligator.png';
+import Help from './assets/help_text.png'
 
-function App() {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.hoverMouse = null;
+    this.state = {
+    };
+  }
+  componentDidMount(){
+    this.hoverMouse = document.addEventListener("mousemove", getMouse);
+    var bee = document.getElementById("bee");
+    var mouse = {x:0, y:0}; //mouse.x, mouse.y
+    bee.style.position = "absolute";
+    bee.style.zIndex="9000"; //css		
+    var beepos = {x:0, y:0};
+    setInterval(followMouse, 50);
+		var dir = "right";
+    function getMouse(e){
+        mouse.x = e.pageX;
+        mouse.y = e.pageY;
+    //Checking directional change
+    if(mouse.x > beepos.x){
+      dir = "right";
+    } else {
+      dir = "left";
+    }
+    }
+    function followMouse(){
+			//1. find distance X , distance Y
+			var distX = mouse.x - beepos.x;
+			var distY = mouse.y - beepos.y;
+			//Easing motion
+       //Progressive reduction of distance 
+			beepos.x += distX/5;
+			beepos.y += distY/2;
+			
+			bee.style.left = beepos.x + "px";
+			bee.style.top = beepos.y + "px";
+      
+      
+        //Apply css class 
+        if (dir === "right"){
+          bee.setAttribute("class", "right");
+        } else {
+          bee.setAttribute("class", "left");        
+        }
+		} 
+  }
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+      <div id="bee"> 
+  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/160783/astroboy.png"  alt="Astroboy Mouse Icon"/>
+</div>
+        {/*<figure>
+        <img src={Hero} alt="Asian Girl piggy-backing black friend and enjoying themselves" width="1440" height="820" />
+        <figcaption><img src={Coming} width="1190" height="174" alt="Open Mics Coming Soon"/></figcaption>
+        </figure>*/}
+        <iframe title="Graphic" src="./paper/examples/Paperjs.org/NyanRainbow.html" width="1920" height="1080"></iframe>
+      </main>
+      <footer className="footer-content">
+        <section>
+          <img src={FooterLogo} alt="Stand Up Seoul Company Logo" width="531" height="380"/>
+        </section>
+        <section className="contact-form">
+          <img src={Help} alt="Contact Welcome Text - How can we help?" width="309" height="55"/>
+          <form className="email-form">
+            <label for="fName" />
+            <input type="text" id="fName" name="fName" placeholder="Full Name"/><br/>
+            <label for="email" />
+            <input type="email" id="email" name="email" placeholder="Email address"/><br/>
+            <label for="message" />
+            <textarea name="message" id="message" placeholder="What's on your mind?"/><br/>
+            <input type="submit" value="Submit" />
+          </form>
+          <small>Stand Up Seoul &copy; 2021 All rights reserved.</small>
+        </section>
+      </footer>
     </div>
   );
 }
+}
 
-export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,76 @@
+import React from "react";
+import Colorlogo from '../assets/SUSOfficial.png';
+import Whitelogo from '../assets/header_logo_w.png';
+import './header.css'
+
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.hoverMouse = null;
+    this.listener = null;
+    this.state = {
+      status: "top",
+      isOpen: false
+    };
+  }
+
+  componentDidMount() {
+    this.listener = document.addEventListener("scroll", e => {
+      var scrolled = document.scrollingElement.scrollTop;
+      if (scrolled >= 1) {
+        if (this.state.status !== "amir") {
+          this.setState({ status: "amir" });
+        }
+      } else {
+        if (this.state.status !== "top") {
+          this.setState({ status: "top" });
+        }
+      }
+    });
+  }
+
+  componentDidUpdate() {
+    document.removeEventListener("scroll", this.listener);
+  }
+
+  handleClick = () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".main-nav");
+    console.log("Abc");
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active")
+  }
+
+  render() {
+    return (
+        <div>
+            <header className="App-header"
+        style={{
+            backgroundColor: "#000",
+            color: "white",
+            position: "fixed",
+            transition: "all 0.1s linear"
+          }}>
+              <div className="hamburger" onClick={this.handleClick}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+              <nav className="main-nav">
+            <ul>
+            <li>ABOUT</li>
+            <li>SHOWS (COMING SOON)</li>
+            <li>SHOP (COMING SOON)</li>
+          </ul>
+        </nav>
+        <a href="/" className="main-logo">
+          <img src= {this.state.status === "top" ? Colorlogo : Whitelogo} alt="Stand Up Seoul Company Logo" width="364"height="35" />
+          </a>
+      </header>
+        </div>
+    );
+  }
+}
